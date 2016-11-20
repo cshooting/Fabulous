423dcabb479f6950c35251598db51cf57e8c831d
张Keith <Keith>
2016-11-20 11:39:38 +0800

    Initial commit

---
+++ app/src/main/java/com/sports/a511/fabulous/MeFragment.java
@@ -0,0 +1,22 @@
+package com.sports.a511.fabulous;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by keith on 2016/11/19.
+ */
+
+public class MeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mtab_me,container,false);
+
+    }
+}
